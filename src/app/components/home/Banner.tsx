595057b94bb493b239555f6f848bdaeb8a9e5260
AlvinBrianda <alvinbrianda23@gmail.com>
2025-08-05 13:11:03 +0700

    Update Home

--- src/app/components/home/Banner.tsx
+++ src/app/components/home/Banner.tsx
@@ -6,7 +6,7 @@ import React from 'react';
 const HeroBanner: React.FC = () => {
   return (
     <section className="bg-gradient-to-r from-[#0f0f0f] via-[#0f0f0f] to-orange-900 text-white py-16 md:py-24">
-      <div className="container mx-auto px-4 sm:px-6 md:px-1 pt-[40px] md:pt-[20px] pb-[10px] md:pb-[5 px] flex flex-col gap-[8px] sm:gap-[10px] md:gap-[16px]">
+      <div className="container mx-auto px-[120px] pt-[40px] md:pt-[20px] pb-[10px] md:pb-[5px] flex flex-col gap-[100px]">
 
         <div className="max-w-6xl text-left">
           <h1 className="text-3xl sm:text-4xl md:text-5xl font-medium leading-snug mb-6" style={{ fontFamily: 'Poppins' }}>
@@ -18,7 +18,7 @@ const HeroBanner: React.FC = () => {
             yang mendukung perkembangan bisnis <span className="text-white">Anda.</span>
           </h1>
 
-          <p className="text-gray-300 max-w-6xl mb-30 text-base md:text-Z" style={{ fontFamily: 'Poppins' }}>
+          <p className="text-gray-300 max-w-6xl mb-30 text-base md:text-lg" style={{ fontFamily: 'Poppins' }}>
             Layanan konsultasi IT dan pengembangan sistem terintegrasi yang dirancang untuk efisiensi, inovasi, dan dampak nyata
           </p>
 
