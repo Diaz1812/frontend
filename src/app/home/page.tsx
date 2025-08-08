f252f38082ec5436bed80f4288d951b8fdf33769
Muhammad Diaz Alwi <alwidiaz502@gmail.com>
2025-08-08 10:50:30 +0700

    updated

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,52 +1,48 @@
-import Header from '../components/home/Header'
-import HeroBanner from '../components/home/Banner'
-import AboutSection from '../components/home/AboutUs'
-import OurService from '../components/home/OurService'
-import Footer from '../components/home/Footer'
-import HomeSection from '../components/home/Portofolio'
-import WhyMicrodata from '../components/home/WhyMicrodata'
-import ClientExperience from '../components/home/OurClient'
-import ProcessSection from '../components/home/OurProcess'
-import VisiMisi from '../components/home/VisiMisi'
-import LogoSlide from '../components/home/LogoSlide'
-import Teknologi from '../components/home/Teknologi'
-import ContactUs from '../components/home/ContactUs'
-import '../../app/globals.css'
-import LiniBisnis from '../components/home/LiniBisnis'
-
-
+import Header from "../components/home/Header";
+import HeroBanner from "../components/home/Banner";
+import AboutSection from "../components/home/AboutUs";
+import OurService from "../components/home/OurService";
+import Footer from "../components/home/Footer";
+import HomeSection from "../components/home/Portofolio";
+import WhyMicrodata from "../components/home/WhyMicrodata";
+import ClientExperience from "../components/home/OurClient";
+import ProcessSection from "../components/home/OurProcess";
+import VisiMisi from "../components/home/VisiMisi";
+import LogoSlide from "../components/home/LogoSlide";
+import Teknologi from "../components/home/Teknologi";
+import ContactUs from "../components/home/ContactUs";
+import "../../app/globals.css";
+import LiniBisnis from "../components/home/LiniBisnis";
 
 function HomePage() {
   return (
     <div className="min-h-screen bg-white">
       <Header />
-      
-    
+
       {/* Main Content */}
       <main>
-
-      {/* Banner */}
+        {/* Banner */}
         <HeroBanner />
 
-      {/* AboutSection */}
+        {/* AboutSection */}
         <AboutSection />
 
-      {/* VisiMisi */}
+        {/* VisiMisi */}
         <VisiMisi />
 
-      {/* OurService */}
+        {/* OurService */}
         <OurService />
 
-      {/* LogoSlide */}
+        {/* LogoSlide */}
         <LogoSlide />
 
-       {/* HomeSection */}
+        {/* HomeSection */}
         <HomeSection />
 
-       {/* TeknologiSection */}
+        {/* TeknologiSection */}
         <Teknologi />
 
-       {/* WhyMicrodata */}
+        {/* WhyMicrodata */}
         <WhyMicrodata />
 
         {/* ClientExperience */}
@@ -60,13 +56,12 @@ function HomePage() {
 
         {/* LiniBisnis */}
         <LiniBisnis />
-
       </main>
 
       {/* Footer */}
       <Footer />
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
