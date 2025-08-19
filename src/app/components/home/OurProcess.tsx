c5200e6b452b2ec386cec43e3db8344357b8df09
AlvinBrianda <alvinbrianda23@gmail.com>
2025-08-19 16:49:04 +0700

    update

--- src/app/components/home/OurProcess.tsx
+++ src/app/components/home/OurProcess.tsx
@@ -20,7 +20,7 @@ const steps: Step[] = [
   },
   {
     icon: <Rocket className="w-8 h-8 text-gray-500" />,
-    title: "Testing & Implementasii",
+    title: "Testing & Implementasi",
     desc: "Proses Testing, Pembayaran dan Implementasi",
   },
 ];
