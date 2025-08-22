9026eb9d81df8c1e9ed87a1502e6f5b8693c8931
Muhammad Diaz Alwi <alwidiaz502@gmail.com>
2025-08-22 15:31:33 +0700

    update banner home

--- src/app/components/home/Banner.tsx
+++ src/app/components/home/Banner.tsx
@@ -18,8 +18,8 @@ const dancingScript = Dancing_Script({
 const HeroBanner: React.FC = () => {
   return (
     <section className="bg-gradient-to-r from-[#0f0f0f] via-[#0f0f0f] to-orange-900 text-white py-16 md:py-24">
-      <div className="container mx-auto px-4 sm:px-6 md:px-8 lg:px-[120px] pt-[40px] md:pt-[20px] pb-[10px] md:pb-[5px] flex flex-col gap-[100px]">
-        <div className="max-w-7xl text-left">
+      <div className="container mx-auto px-4 sm:px-6 md:px-[120px] pt-[40px] md:pt-[90px] pb-[10px] md:pb-[5px] flex flex-col gap-[100px]">
+        <div className="max-w-6xl text-left">
           <h1
             className={`text-3xl sm:text-4xl md:text-5xl font-medium leading-snug mb-6 ${poppins.className}`}
           >
@@ -31,12 +31,14 @@ const HeroBanner: React.FC = () => {
             yang mendukung perkembangan bisnis{" "}
             <span className="text-white">Anda.</span>
           </h1>
+
           <p
             className={`text-gray-300 max-w-6xl mb-15 text-base md:text-lg ${poppins.className}`}
           >
             Layanan konsultasi IT dan pengembangan sistem terintegrasi yang
             dirancang untuk efisiensi, inovasi, dan dampak nyata
           </p>
+
           <div className="flex flex-row flex-wrap gap-4 justify-start">
             <Link href="/contact">
               <button className="bg-orange-500 hover:bg-orange-600 text-white font-medium py-2 px-5 rounded-full transition w-full sm:w-auto">
